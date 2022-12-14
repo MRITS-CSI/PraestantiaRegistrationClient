54fea5ba1d995fbeb7f8e552728a791bce846995
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2022-12-14 11:26:57 +0530

    edits

--- script.js
+++ script.js
@@ -1,77 +1,96 @@
-document.getElementById("player2").style.display = "none";
+document.getElementById('player2').style.display = 'flex';
 let isTeam = false;
-document.getElementById("game").value = 'single';
+document.getElementById('game').value = 'team';
 class Player {
-    constructor(name, branch, yearOfStudy, section, rollNo, phoneNo, email) {
-        this.name = name;
-        this.branch = branch;
-        this.yearOfStudy = yearOfStudy;
-        this.section = section;
-        this.rollNo = rollNo;
-        this.phoneNo = phoneNo;
-        this.email = email;
-    }
+	constructor(name, branch, yearOfStudy, section, rollNo, phoneNo, email) {
+		this.name = name;
+		this.branch = branch;
+		this.yearOfStudy = yearOfStudy;
+		this.section = section;
+		this.rollNo = rollNo;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
 }
 
-
-document.getElementById("game").addEventListener("change", function() {
-    if (this.value == "team") {
-        document.getElementById("player2").style.display = "flex";
-        isTeam = true;
-    } else {
-        document.getElementById("player2").style.display = "none";
-        isTeam = false;
-    }
+document.getElementById('game').addEventListener('change', function () {
+	if (this.value == 'team') {
+		document.getElementById('player2').style.display = 'flex';
+		isTeam = true;
+	} else {
+		document.getElementById('player2').style.display = 'none';
+		isTeam = false;
+	}
 });
 
-document.getElementById("submit").addEventListener("click", function() {
-    submit();
+document.getElementById('submit').addEventListener('click', function () {
+	submit();
 });
 
-console.log(JSON.stringify([new Player("n", "b", "1", "s", "r", "p", "e")]))
+console.log(JSON.stringify([new Player('n', 'b', '1', 's', 'r', 'p', 'e')]));
 
 function submit() {
-    let player1 = new Player(
-        document.getElementById("name1").value,
-        document.getElementById("branch1").value,
-        document.getElementById("year1").value,
-        document.getElementById("section1").value,
-        document.getElementById("htno1").value,
-        document.getElementById("phoneno1").value,
-        document.getElementById("email1").value
-    );
-    let player2 = isTeam ? new Player(
-        document.getElementById("name2").value,
-        document.getElementById("branch2").value,
-        document.getElementById("year2").value,
-        document.getElementById("section2").value,
-        document.getElementById("htno2").value,
-        document.getElementById("phoneno2").value,
-        document.getElementById("email2").value
-    ) : null;
+	let player1 = new Player(
+		document.getElementById('name1').value,
+		document.getElementById('branch1').value,
+		document.getElementById('year1').value,
+		document.getElementById('section1').value,
+		document.getElementById('htno1').value,
+		document.getElementById('phoneno1').value,
+		document.getElementById('email1').value
+	);
+	let player2 = isTeam
+		? new Player(
+				document.getElementById('name2').value,
+				document.getElementById('branch2').value,
+				document.getElementById('year2').value,
+				document.getElementById('section2').value,
+				document.getElementById('htno2').value,
+				document.getElementById('phoneno2').value,
+				document.getElementById('email2').value
+		  )
+		: null;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://csi-mrits.tech/api/v1/user/signup")
-    xhr.setRequestHeader("Content-Type", "application/json");
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'https://csi-mrits.tech/api/v1/user/signup');
+	xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-            document.getElementById("teamNumber").innerText = "Team number: " + JSON.parse(xhr.responseText).msg.teamNo;
-            document.getElementById("password").innerText = "Password: " + JSON.parse(xhr.responseText).password;
-        }
-    };
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+			document.getElementById('teamNumber').innerText =
+				'Team number: ' + JSON.parse(xhr.responseText).msg.teamNo;
+			document.getElementById('password').innerText =
+				'Password: ' + JSON.parse(xhr.responseText).password;
+		}
+	};
 
-    let data = (isTeam && player2) ? JSON.stringify([player1, player2]) : JSON.stringify([player1]);
+	let data =
+		isTeam && player2
+			? JSON.stringify([player1, player2])
+			: JSON.stringify([player1]);
 
-    let jsondata = `{"players":${data}}`;
+	let jsondata = `{"players":${data}}`;
 
-    xhr.send(jsondata);
-    console.log("sent");
+	xhr.send(jsondata);
+	console.log('sent');
+	document.getElementById('name1').value = '';
+	document.getElementById('branch1').value = '';
+	document.getElementById('year1').value = '';
+	document.getElementById('section1').value = '';
+	document.getElementById('htno1').value = '';
+	document.getElementById('phoneno1').value = '';
+	document.getElementById('email1').value = '';
+
+	document.getElementById('name2').value = '';
+	document.getElementById('branch2').value = '';
+	document.getElementById('year2').value = '';
+	document.getElementById('section2').value = '';
+	document.getElementById('htno2').value = '';
+	document.getElementById('phoneno2').value = '';
+	document.getElementById('email2').value = '';
 }
-
-
 
 // function generatePlayerField(n) {
 //     // create a textbox named "name" with id "name1"
@@ -105,7 +124,6 @@ function submit() {
 //         year.appendChild(opt);
 //     }
 
-//     //add select named 
-
+//     //add select named
 
 // }
